078a1cbcb6bc7faf90176a7755a1cda09c81f5f4
erinnivv <erin.govender09@gmail.com>
2026-02-17 11:49:23 +0200

    Two player keyboard

---
+++ Classwork/Assets/Scripts/TwoPlayersOneKeyboard.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TwoPlayersOneKeyboard : MonoBehaviour
+{
+
+    [Header("Actions (drag from your Input Actions Asset)")]
+    [SerializeField] private InputActionReference p1Move;
+    [SerializeField] private InputActionReference p2Move;
+
+    [Header("Players")]
+    [SerializeField] private Transform p1;
+    [SerializeField] private Transform p2;
+
+    [SerializeField] private float speed = 5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void OnEnable()
+    {
+        p1Move.action.Enable();
+        p2Move.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        p1Move.action.Disable();
+        p2Move.action.Disable();
+    }
+
+    private void Update()
+    {
+        var m1 = p1Move.action.ReadValue<Vector2>();
+        var m2 = p2Move.action.ReadValue<Vector2>();
+
+        if (p1) p1.position += new Vector3(m1.x, 0f, m1.y) * speed * Time.deltaTime;
+        if (p2) p2.position += new Vector3(m2.x, 0f, m2.y) * speed * Time.deltaTime;
+    }
+}
